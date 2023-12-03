f53509b9049f04b412ac0f27cbca282d238a950b
tsuengineer <ootubom@gmail.com>
2023-12-03 22:55:39 +0900

    マイグレーションファイルを作成

---
+++ database/migrations/2023_12_03_212018_create_facemarks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('facemarks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->ulid()->unique()->comment('ULID');
+            $table->string('data')->comment('顔文字');
+            $table->integer('copy_count')->default(0);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
